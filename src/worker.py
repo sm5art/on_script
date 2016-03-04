e5ecc882ba3dd0439bdf38326e45d7734cb9d91e
Koding <art@network.ru>
2016-03-04 03:45:21 +0000

    added worker class

---
+++ src/worker.py
@@ -0,0 +1,13 @@
+class Worker:
+    def __init__(self,job = None):
+        self.job = job
+        self.parse_input()
+        
+    def parse_input():
+        if isinstance(self.job,basestring):
+            try:
+                if 
+            except(IOError,OSError) as e:
+                print e
+                
+                
